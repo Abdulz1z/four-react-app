30e6d2a25956eb11169d58e9c72dc5b2aecbc6c4
Abdulaziz obidov <115730333+Abdulz1z@users.noreply.github.com>
2024-02-13 04:13:05 -0700

    New Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Fitch from "./components/Fitch";
+function App() {
+    return (
+        <div>
+            HEllo World
+            <Fitch />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Fitch.jsx
@@ -0,0 +1,6 @@
+function Fitch() {
+    fetch();
+    return <div>fitch</div>;
+}
+
+export default Fitch;
